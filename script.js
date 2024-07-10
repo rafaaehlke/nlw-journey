a29560f82c63f0b7cf27a891f317ffcc8ee94e5e
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-07-10 20:45:00 -0300

    finalizada aula 02

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ let atividades = [
   {
     name: 'Academia em Grupo',
     date: new Date('2024-07-09 10:00'),
-    finish: true
+    finish: false
   },
   {
     name: 'Gaming Session',
@@ -46,7 +46,12 @@ let atividades = [
 //Arrow Function
 const criarItemAtividades = (atividade) => {
 
-  let input = '<input type="checkbox" ' //criada variavel para o input checkbox
+  let input = `
+   <input
+   onchange="concluirAtividade(event)"
+   value="${atividade.date}"
+   type="checkbox"
+    ` //criada variavel para o input checkbox
 
   if (atividade.finish) { //Se a atividade estiver finalizada, adiciona o checked 
     input += 'checked'
@@ -73,7 +78,7 @@ const criarItemAtividades = (atividade) => {
 
 const atualizaListaDeAtividades = () => {
   const section = document.querySelector('section')
-  section.innerHTML = ''
+  section.innerHTML = '' //apagamos a lista toda, para quando atulizar com a nova atividade
 
 
   //Verifica se a lista esta vazia
@@ -91,28 +96,35 @@ atualizaListaDeAtividades()
 
 //cancela o evento de envio automatico do formulário
 const salvarAtividade = (event) => {
- event.preventDefault()
-  const dadosDoFormulario = new FormData(event.target)
-  console.log("teste")
+  event.preventDefault()
+  const dadosDoFormulario = new FormData(event.target) //pegamos o formulario alvo
 
+  //dados do formulário
   const name = dadosDoFormulario.get('atividade')
   const dia = dadosDoFormulario.get('dia')
   const hora = dadosDoFormulario.get('hora')
   const date = `${dia} ${hora}`
 
   
-  const atividade = {
-    name,
-    date,
-    finish: true
+  const novaAtividade = { 
+    name, //quando o nome da propriedade for o mesmo nome da variavel, podemos deixar apenas 1 nome igual ao lado
+    date, //verrsao sem simplificar ficaria date: date,
+    finish: false
   }
   
-  atividades = [atividade, ...atividades]
+   const atividadeExiste = atividades.find((atividade) => {
+     return atividade.date == novaAtividade.date
+   })
+
+   if(atividadeExiste) {
+    return alert('Voce já marcou uma atividade nesse horário')
+   }
+
+  atividades = [novaAtividade, ...atividades] //quando usamos ... colocamos as atividades antigas na lista! e quando adicionamos atividade na frente, faz com que
+  //adicione a nova atividade na frente das antigas
   atualizaListaDeAtividades()
 
 }
-
-//Parte do Formulario
 
 //cria e formata os dias da viagem para dia e mes
 const criarDiasSelecao = () => {
@@ -147,11 +159,30 @@ const criarHorasSelecao = () => {
 
   let horasDisponiveis = ''
 
-  for(let i = 6; i < 24; i++) {
-    horasDisponiveis += `<option value="${i}:00">${i}:00</option>`
-    horasDisponiveis += `<option value="${i}:30">${i}:30</option>`
+  //loop para criar as opcoes de horas
+  for(let i = 6; i < 24; i++) {  //começa do 6, ate que seja menor que 24, depois encrementa
+    const hora = String(i).padStart(2, "0")
+    horasDisponiveis += `<option value="${hora}:00"> ${hora}:00 </option>`
+    horasDisponiveis += `<option value="${hora}:30"> ${hora}:30 </option>`
   }
 
   document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis
 }
 criarHorasSelecao()
+
+//faz com que permanca o check caso esteja marcado ou nao!
+  const concluirAtividade = (event) => {
+  const input = event.target
+  const dataDoInput = input.value
+  
+
+   const atividade = atividades.find((atividade) => {
+    return atividade.date == dataDoInput
+   })
+
+   if(!atividade) {
+    return
+  }
+
+   atividade.finish = !atividade.finish 
+}
