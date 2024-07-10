24c3f6207a6510c689e7d253107ec58aeda3cfbe
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-07-09 22:32:18 -0300

    criada funcao adicionar atividades pelo formulario

--- script.js
+++ script.js
@@ -71,8 +71,10 @@ const criarItemAtividades = (atividade) => {
 }
 
 
-const atulizaListaDeAtividades = () => {
+const atualizaListaDeAtividades = () => {
   const section = document.querySelector('section')
+  section.innerHTML = ''
+
 
   //Verifica se a lista esta vazia
   if (atividades.length == 0) {
@@ -85,5 +87,71 @@ const atulizaListaDeAtividades = () => {
   }
 
 }
+atualizaListaDeAtividades()
 
-atulizaListaDeAtividades()
+//cancela o evento de envio automatico do formulário
+const salvarAtividade = (event) => {
+ event.preventDefault()
+  const dadosDoFormulario = new FormData(event.target)
+  console.log("teste")
+
+  const name = dadosDoFormulario.get('atividade')
+  const dia = dadosDoFormulario.get('dia')
+  const hora = dadosDoFormulario.get('hora')
+  const date = `${dia} ${hora}`
+
+  
+  const atividade = {
+    name,
+    date,
+    finish: true
+  }
+  
+  atividades = [atividade, ...atividades]
+  atualizaListaDeAtividades()
+
+}
+
+//Parte do Formulario
+
+//cria e formata os dias da viagem para dia e mes
+const criarDiasSelecao = () => {
+  const dias = [
+    "2024-08-25",
+    "2024-08-26",
+    "2024-08-27",
+    "2024-08-28",
+  ]
+
+  let diasSelecao = ''
+
+  for(let dia of dias) {
+    const formatar = formatador(dia)
+    const diaFormatado = `
+    ${formatar.dia.numerico} de 
+    ${formatar.mes}
+    `
+
+    diasSelecao += `
+      <option value="${dia}">
+      ${diaFormatado}
+      </option>
+    `
+  }
+
+  document.querySelector('select[name="dia"]').innerHTML = diasSelecao
+}
+criarDiasSelecao()
+
+const criarHorasSelecao = () => {
+
+  let horasDisponiveis = ''
+
+  for(let i = 6; i < 24; i++) {
+    horasDisponiveis += `<option value="${i}:00">${i}:00</option>`
+    horasDisponiveis += `<option value="${i}:30">${i}:30</option>`
+  }
+
+  document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis
+}
+criarHorasSelecao()
